403b6a4aafd29fd4b1418b917f0805bc74f8736c
Muflih majid <bsi80113@bsi.co.id>
2024-02-12 12:53:03 +0700

    This commit for commiting interface and url and also package what will use by application

---
+++ src/test/java/com/CleanArc/ProjectNasa/Infrastructure/Utils/GlobalConstanta.java
@@ -0,0 +1,9 @@
+package com.CleanArc.ProjectNasa.Infrastructure.Utils;
+
+// this class is used to store global constanta
+public class GlobalConstanta {
+    public static final String NEO_FEED_URL = "https://api.nasa.gov/neo/rest/v1/feed?";
+	
+	public static final String NEO_LOOKUP_URL = "https://api.nasa.gov/EPIC/api/natural/images?api_key=4l0IAfnPlmmI1b73TiUPWnFaKTaFkTpr9DOCosGn";
+	   
+}
